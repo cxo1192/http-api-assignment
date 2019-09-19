c69c59eaf10c39c69035c7d9102820625a59c426
cxo1192 <cxo1192@g.rit.edu>
2019-09-19 16:00:59 -0400

    fixed loading css issue dropdoown still does not work

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
   '/': htmlHandler.getIndex,
+  '/style.css': htmlHandler.getCSS,
   '/success': jsonHandler.success,
   '/badRequest': jsonHandler.badRequest,
   '/unauthorized': jsonHandler.unauthorized,
