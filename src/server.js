ce7df9dbd4528f927ba67b9f0987d1958ba98b10
cxo1192 <cxo1192@g.rit.edu>
2019-09-19 15:54:53 -0400

    direct url requests successful, dropdown fails

---
+++ src/server.js
@@ -0,0 +1,77 @@
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+  '/': htmlHandler.getIndex,
+  '/success': jsonHandler.success,
+  '/badRequest': jsonHandler.badRequest,
+  '/unauthorized': jsonHandler.unauthorized,
+  '/forbidden': jsonHandler.forbidden,
+  '/internal': jsonHandler.internal,
+  '/notImplemented': jsonHandler.notImplemented,
+  notFound: jsonHandler.notFound,
+};
+
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+  const params = query.parse(parsedUrl.query);
+
+  if (urlStruct[parsedUrl.pathname]) {
+    urlStruct[parsedUrl.pathname](request, response, params);
+  } else {
+    urlStruct.notFound(request, response, params);
+  }
+};
+
+
+// const handlePost = (request, response, parsedUrl) => {
+//   if (parsedUrl.pathname === '/addUser') {
+//     const body = [];
+
+//     request.on('error', (err) => {
+//       console.dir(err);
+//       response.statusCode = 400;
+//       response.end();
+//     });
+
+//     request.on('data', (chunk) => {
+//       body.push(chunk);
+//     });
+
+//     request.on('end', () => {
+//       const bodyString = Buffer.concat(body).toString();
+//       const bodyParams = query.parse(bodyString);
+
+//       jsonHandler.addUser(request, response, bodyParams);
+//     });
+//   }
+// };
+
+// const handleGet = (request, response, parsedUrl) => {
+//   if (parsedUrl.pathname === '/style.css') {
+//     htmlHandler.getCSS(request, response);
+//   } else if (parsedUrl.pathname === '/getUsers') {
+//     jsonHandler.getUsers(request, response);
+//   } else {
+//     htmlHandler.getIndex(request, response);
+//   }
+// };
+
+// const onRequest = (request, response) => {
+//   const parsedUrl = url.parse(request.url);
+
+//   if (request.method === 'POST') {
+//     handlePost(request, response, parsedUrl);
+//   } else {
+//     handleGet(request, response, parsedUrl);
+//   }
+// };
+
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);
